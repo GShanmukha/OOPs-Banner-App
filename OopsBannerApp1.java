692ac04109f197761f1f697a4d23859e64f8eee5
GORRE SHANMUKHA SAI <sg1797@srmist.edu.in>
2026-03-10 12:54:27 +0530

    oopsbannerapp8

--- OopsBannerApp1.java
+++ OopsBannerApp1.java
@@ -1,5 +1,18 @@
-public class OopsBannerApp1{
-    public static void main(String[] args){
-        System.out.println("OOPS ");
+public class OOPSBANNERAPP1 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("",  "  ***   ", "  ***   ", "******  ", "  ***** "));
+        
+        System.out.println(String.join("",  " **  ** ", " **  ** ", "**    **", " **     "));
+        
+        System.out.println(String.join("",  "**    **", "**    **", "**    **", " **      "));
+        
+        System.out.println(String.join("",  "**    **", "**    **", "**    **", " *****   "));
+        
+        System.out.println(String.join("", "**    **", "**    **", " ****** ", "    **   "));
+        
+        System.out.println(String.join("",    " **  ** ", " **   **", "**      ", "    **  "));
+        
+        System.out.println(String.join("",   " ***  ", "   ***  ", "**      ", " *****   "));
     }
 }
